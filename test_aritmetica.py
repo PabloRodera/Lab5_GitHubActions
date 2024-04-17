862f61fce76ba16dae6e5a4b8970ae9cfb7823bb
PabloRodera <pablo_rodera@hotmail.com>
2024-04-17 11:24:43 +0200

    laboratorio 5 realizado, commit + push de prueba

---
+++ test_aritmetica.py
@@ -0,0 +1,30 @@
+import unittest
+from arimetica import sumar, restar, multiplicar, dividir
+
+class TestSumar(unittest.TestCase):
+    def test_sumar(self):
+        self.assertEqual(sumar(3, 2), 5)
+        self.assertEqual(sumar(-1, 1), 0)
+        self.assertEqual(sumar(-1, -1), -2)
+
+    def test_restar(self):
+        self.assertEqual(restar(5, 3), 2)
+        self.assertEqual(restar(1, 3), -2)
+        self.assertEqual(restar(-1, -1), 0)
+    
+    def test_multiplicar(self):
+        self.assertEqual(multiplicar(3, 2), 6)
+        self.assertEqual(multiplicar(-1, 1), -1)
+        self.assertEqual(multiplicar(-1, -1), 1)
+
+    def test_dividir(self):
+        self.assertEqual(dividir(8, 4), 2)
+        self.assertEqual(dividir(9, 3), 3)
+        self.assertEqual(dividir(-6, 3), -2)
+        self.assertEqual(dividir(0, 5), 0)
+
+        with self.assertRaises(ValueError):
+            dividir(6, 0)
+            
+if __name__ == '__main__':
+    unittest.main()
